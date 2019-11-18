b013648b9c5e524cbcdf8c22313eac2cfd4de2f2
Bill White <bill.white@griggsinst.com>
2019-11-17 20:49:01 -0800

    Make abc2pdf.sh operate on several files.

--- tools/abc2pdf.sh
+++ tools/abc2pdf.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 set -x
-FILE="$(basename "$1" .abc)"
 TMP=./tmp
 OUT=./output
 DATE="$(date --iso=seconds)"
+rm -rf "$TMP" "$OUT"
 mkdir -p "$TMP"
 mkdir -p "$OUT"
+mkdir -p "$OUT/src"
+mkdir -p "$OUT/PDF"
 sed -e "s/@DATE@/$DATE/" < stamp.ps | ps2pdf - "$TMP/stamp.pdf"
-abcm2ps -F fbook1.fmt -j 0b -O- "${FILE}.abc" | ps2pdf - - | pdftk - stamp $TMP/stamp.pdf output "${OUT}/${FILE}.pdf"
 
+for IF in "$@" ; do
+    FILE="$(basename "$IF" .abc)"
+    cp "${FILE}.abc" "$OUT/src"
+    abcm2ps -F fbook1.fmt -j 0b -O- "${FILE}.abc" | ps2pdf - - | pdftk - stamp $TMP/stamp.pdf output "${OUT}/PDF/${FILE}.pdf"
+done
